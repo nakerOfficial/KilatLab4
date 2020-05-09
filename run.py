8824060ca74e3ee5c0e8c1651428c0ad970e889c
nakerOfficial <33634990+nakerOfficial@users.noreply.github.com>
2020-05-09 17:30:45 +0300

    v2

--- run.py
+++ run.py
@@ -1,12 +1,30 @@
-text = "1"
+text = 'abcc'
 
-calcs = 0
-for a in text:
-    calcs += ord(a) * len(text)
+def getHex_32(number):
+    hexList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f']
+    hexList = hexList + hexList[::-1]
 
-while len(str(calcs)) < 32:
-    calcs *= calcs
+    while number >= 32:
+        number -= 32
 
-result = str(calcs)[0:32]
+    return str(hexList[number])
 
-print("Result: ", result)
+def hashCalc(text):
+    length = len(text)
+    result = ''
+
+    Hash = 0
+    for a in text:
+        Hash = (Hash + length * ord(a))
+
+    Hash = Hash // length
+    for a in range(32):
+        Hash = Hash + length
+        result += getHex_32(Hash)
+
+    return result
+
+print(hashCalc(text))
+
+# 'abcd' ed95126aed95126aed95126aed95126a
+# 'abcc' dea62159dea62159dea62159dea62159
